4a341534d17b6af5388cb7d9303095ab3848e621
Jaegyeong <endercraft09@khu.ac.kr>
2022-11-20 22:10:36 +0900

    Add Volume Setting UI

---
+++ Assets/Scripts/UI/SetVolume.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SetVolume : MonoBehaviour
+{
+    public AudioMixer mixer;
+    public Slider slider;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("masterVol", 0.75f);
+    }
+
+    public void SetLevel(float sliderValue)
+    {
+        mixer.SetFloat("masterVol", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("masterVol", sliderValue);
+    }
+}
