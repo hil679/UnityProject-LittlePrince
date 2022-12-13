9411a61113371c5cea28a0cde8e458f1e8f07062
SNG0407 <ksc0407@khu.ac.kr>
2022-12-14 08:21:26 +0900

    이동씬 없앰

--- Assets/Scripts/TeleportManager.cs
+++ Assets/Scripts/TeleportManager.cs
@@ -28,13 +28,13 @@ public class TeleportManager : MonoBehaviour
             
             Scene scene = SceneManager.GetActiveScene();
             if (scene.name == "Earth_plane")
-                loader.LoadScene("UniverseEarthToDinosour");
+                loader.LoadScene("Dino_Sample");
             
             else if (scene.name == "Dino_Sample")
-                loader.LoadScene("UniverseDinoSourToDesert");
+                loader.LoadScene("PathInDesert_Sample");
             
             else if (scene.name == "PathInDesert_Sample")
-                loader.LoadScene("UniverseDesertToEarth");
+                loader.LoadScene("FinalScene");
         }
         
     }
